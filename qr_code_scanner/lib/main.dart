139b5f1d777c51f41d5efbb52a53752074e62e3e
devpool007 <sharmadevansh007777@gmail.com>
2019-03-08 01:23:17 +0530

    done with basics

--- qr_code_scanner/lib/main.dart
+++ qr_code_scanner/lib/main.dart
@@ -1,12 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: HomePage(),
-));
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class HomePage extends StatelessWidget { 
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  String result = "Hey there !";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "Camera permission was denied";
+        });
+      } else {
+        setState(() {
+          result = "Unknown Error $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "You pressed the back button before scanning anything";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Unknown Error $ex";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,17 +53,17 @@ class HomePage extends StatelessWidget {
         title: Text("QR Scanner"),
       ),
       body: Center(
-      child:Text("Hey there!",style: new TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
       ),
-    floatingActionButton: FloatingActionButton.extended(
-      icon: Icon(Icons.camera_alt),
-      label: Text("Scan"),
-      onPressed:(){
-        
-      },
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-  } 
+  }
 }
